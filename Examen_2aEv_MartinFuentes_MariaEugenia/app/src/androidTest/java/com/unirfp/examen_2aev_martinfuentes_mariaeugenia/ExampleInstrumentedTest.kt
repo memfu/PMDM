1878e3189c151f2120a98fa3a35772b26dac3635
Maria E. Martin Fuentes <me.martinfuentes@gmail.com>
2025-03-06 13:44:54 +0100

    creacion proyecto examen: manifest+gradle

---
+++ Examen_2aEv_MartinFuentes_MariaEugenia/app/src/androidTest/java/com/unirfp/examen_2aev_martinfuentes_mariaeugenia/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.unirfp.examen_2aev_martinfuentes_mariaeugenia
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.unirfp.examen_2aev_martinfuentes_mariaeugenia", appContext.packageName)
+    }
+}
