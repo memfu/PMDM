3ac94838efd6508d7800afde520d7fbb46dbf1ed
Maria E. Martin Fuentes <me.martinfuentes@gmail.com>
2025-05-08 22:05:16 +0200

    proyecto calculadora agregado

---
+++ calculadora_salario/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AE1_Calculadora_salario_Fuentes"
+include(":app")
+ 
